48546412c999196dd56b3cfb8bdf0914a328c719
Noah Delpit <noah02delpit@gmail.com>
2024-11-27 16:50:02 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants, only: [ :new, :index, :create, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create!(
+ name: "Dishoom",
+ address: "7 Boundary St, London E2 7JE",
+ phone_number: "0044 7123 456789",
+ category: "french"
+)
+
+Restaurant.create!(
+ name: "Pizza East",
+ address: "56A Shoreditch High St, London E1 6PQ",
+ phone_number: "0044 7987 654321",
+ category: "italian"
+)
+
+Restaurant.create!(
+ name: "Dragon d'Or",
+ address: "8 rue de la Soie, Paris",
+ phone_number: "01 43 54 23 31",
+ category: "chinese"
+)
+
+Restaurant.create!(
+ name: "Sushi Shop",
+ address: "23 rue du Commerce, Brussels",
+ phone_number: "02 513 12 43",
+ category: "japanese"
+)
+
+Restaurant.create!(
+ name: "Frites Maison",
+ address: "Place Flagey 12, Brussels",
+ phone_number: "02 511 45 67",
+ category: "belgian"
+)
+
+puts "Finished!"
